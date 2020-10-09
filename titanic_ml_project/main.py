731bda94e78802b8997b481bcb2b3d45ff6a1e37
jorgezafra94 <947@holbertonschool.com>
2020-10-09 16:49:52 -0500

    [update] adding documentation main function

--- titanic_ml_project/main.py
+++ titanic_ml_project/main.py
@@ -12,8 +12,19 @@ from execute_model import execute_m
 def main(path: str = '../data/titanic_data/',
          type: str = typer.Option(
              "RandomForest", help="RandomForest, KNeighbors or LogisticRegression"),
-         debug: bool = False, Super: bool = False):
-    """ This is our main method """
+         debug: bool = False,
+         super: bool = False):
+
+    """
+    This is our method to use typer
+    * default behaviour
+      - ./main.py
+    * to use flags
+      - ./main.py --path mypath --type model_type
+    * to use bool flags we have to call them or we are goin to have them with False
+      - ./main.py --path mypath --type model_type --debug
+      - in this way debug is going ot be True without the flag False
+    """
     # print(path, type, debug, Super) # this print show the arguments
     # conditions to fill the parameters
     list_models = ['RandomForest', 'KNeighbors', 'LogisticRegression', '_']

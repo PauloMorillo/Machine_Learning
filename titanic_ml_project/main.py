4c2ec8612410a4ad0baa6aaca7f665959b72c625
PauloMorillo <pauloan@hotmail.com>
2020-10-14 09:55:08 -0500

    [uodate] added load model with dataframe

--- titanic_ml_project/main.py
+++ titanic_ml_project/main.py
@@ -6,15 +6,18 @@ All begins here
 # ************************************* import of packages ************************************
 import typer
 from execute_model import execute_m
+import os
+import mlflow.sklearn
+from mlflow.entities import ViewType
+from mlflow.tracking.client import MlflowClient
 
 
 # *************************************** MAIN **********************************************
 def main(path: str = '../data/titanic_data/',
-         type: str = typer.Option(
+         model_type: str = typer.Option(
              "RandomForest", help="RandomForest, KNeighbors or LogisticRegression"),
          debug: bool = False,
          super: bool = False):
-
     """
     This is our method to use typer
     * default behaviour
@@ -27,14 +30,23 @@ def main(path: str = '../data/titanic_data/',
     """
     # print(path, type, debug, Super) # this print show the arguments
     # conditions to fill the parameters
-    list_models = ['RandomForest', 'KNeighbors', 'LogisticRegression', '_']
-    if type not in list_models:
-        print("type: model to use in training possible values ", end=" ")
-        # in theory, It's printing with style, but I couldn't try
-        valid = typer.secho("RandomForest, KNeighbors or LogisticRegression",
-                            fg=typer.colors.GREEN, bold=True)
-        exit(0)
-    execute_m(path, debug, type, Super=False)
+    if os.environ["ML_DEPLOY"]:
+        best_model = mlflow.search_runs(experiment_ids="0", run_view_type=ViewType.ACTIVE_ONLY,
+                                        max_results=1, order_by=["metrics.test_F1 DESC"])
+
+        model_id = best_model.loc[:, "run_id"].values[0]
+        model = mlflow.sklearn.load_model("runs:/" + model_id + "/model")
+        print(model.predict([[1, 1, 27.0, 7.0000, 1, 1]]))
+    else:
+        list_models = ['RandomForest', 'KNeighbors', 'LogisticRegression', '_']
+        if model_type not in list_models:
+            print("type: model to use in training possible values ", end=" ")
+            # in theory, It's printing with style, but I couldn't try
+            valid = typer.secho("RandomForest, KNeighbors or LogisticRegression",
+                                fg=typer.colors.GREEN, bold=True)
+            exit(0)
+        execute_m(path, debug, model_type, Super=False)
+
 
 if __name__ == '__main__':
     typer.run(main)
